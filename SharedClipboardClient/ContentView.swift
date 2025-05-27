669a765038819d34d1d1461ecc010305daed20ec
Ilya Bylich <ibylich@gmail.com>
2025-05-27 19:54:39 +0200

    Initial Commit

---
+++ SharedClipboardClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SharedClipboardClient
+//
+//  Created by Ilya on 27/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SharedClipboardClient/SharedClipboardClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedClipboardClientApp.swift
+//  SharedClipboardClient
+//
+//  Created by Ilya on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SharedClipboardClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
